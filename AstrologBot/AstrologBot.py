412d6260300c3d9519afbbf215c88c9ea115ae42
Alexandr Usov <usov_a_s@outlook.com>
2018-06-03 19:46:56 +0300

    Создал класс бота и метод получения обновлений.

--- AstrologBot/AstrologBot.py
+++ AstrologBot/AstrologBot.py
@@ -1,1 +1,33 @@
-print("test")
+import requests
+import json
+import ssl_fix
+from settings import Settings
+
+
+class AstrologBot():
+    """Класс бота
+
+    Принимает запросы на гороскоп, ищет гороскоп в базе гороскопов
+    и отправляет  его пользователю
+    """
+
+    def __init__(self):
+        self.settings = Settings()
+        self.url = self.settings.url + self.settings.token + '/'
+
+    def get_updates(self, url):
+        """Метод запрашивает обновление у сервера Telegram"""
+
+        get_updates_url = url + 'getupdates'
+        r = requests.get(get_updates_url)
+        print(r.json())
+
+
+def main():
+    """Метод выполнения модуля"""
+    astrolog = AstrologBot()
+    astrolog.get_updates(astrolog.url)
+
+
+if __name__ == '__main__':
+    main()
---
+++ AstrologBot/settings.py
@@ -0,0 +1,10 @@
+"""Модуль для хранения настроек для бота AstrologBot"""
+
+
+class Settings:
+    """Класс настроек для бота AstrologBot"""
+
+    def __init__(self):
+
+        self.token = '603308410:AAFRtQ5Lnh5bAz4JIMjyGG_AMJInbRibLgs'
+        self.url = 'https://api.telegram.org/bot'
