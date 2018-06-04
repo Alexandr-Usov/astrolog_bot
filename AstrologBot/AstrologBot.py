ed9da48c5d8cd18a3fc088778779bab584abe213
Alexandr Usov <usov_a_s@outlook.com>
2018-06-04 19:03:09 +0300

    Добавил функции чтения гороскопов из файла и случайного выбора гороскопа

--- AstrologBot/AstrologBot.py
+++ AstrologBot/AstrologBot.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import ssl_fix
+import random
 from settings import Settings
 
 
@@ -12,21 +12,38 @@ class AstrologBot():
     """
 
     def __init__(self):
+        """Метод инициализации класса AstrologBot
+
+        Готовит AP и загружает словарь гороскопа.
+        """
         self.settings = Settings()
         self.url = self.settings.url + self.settings.token + '/'
+
+        # Читение файла гороскопов и передача словаря гороскопов в переменную
+        with open('astrological_predictions.json', 'r',
+                    encoding='utf-8') as self.file:
+            self.predictions = json.load(self.file)
 
     def get_updates(self, url):
         """Метод запрашивает обновление у сервера Telegram"""
 
         get_updates_url = url + 'getupdates'
-        r = requests.get(get_updates_url)
-        print(r.json())
+        self.request = requests.get(get_updates_url)
+        print(self.request.json())
+
+    def get_random_prediction(self, list_prediction):
+        """Метод случайно выбирает гороскоп из списка
+
+        в max_lenght передается длинна списка.
+        """
+        return random.choice(list_prediction)
 
 
 def main():
     """Метод выполнения модуля"""
     astrolog = AstrologBot()
-    astrolog.get_updates(astrolog.url)
+    # astrolog.get_updates(astrolog.url)
+    print(astrolog.get_random_prediction(astrolog.predictions['daily']))
 
 
 if __name__ == '__main__':
