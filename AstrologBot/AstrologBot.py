3cc945578b5b9d4125d80d7cabad791e2b562db1
Alexandr Usov <usov_a_s@outlook.com>
2018-06-12 09:29:45 +0300

    Бот принимает, обрабатывет и отправляет сообщения. Добавленны методы: request_processing, message_processing.

--- AstrologBot/AstrologBot.py
+++ AstrologBot/AstrologBot.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import random
+import time
 from settings import Settings
 
 
@@ -14,34 +15,74 @@ class AstrologBot():
     def __init__(self):
         """Метод инициализации класса AstrologBot
 
-        Готовит AP и загружает словарь гороскопа.
+        Готовит API и загружает словарь гороскопа.
         """
         self.settings = Settings()
         self.url = self.settings.url + self.settings.token + '/'
 
-        # Читение файла гороскопов и передача словаря гороскопов в переменную
-        with open('astrological_predictions.json', 'r',
-                    encoding='utf-8') as self.file:
+        with open('updates_id.txt', 'r') as self.updates_file:
+            self.last_update_id = int(self.updates_file.read())
+
+        # Чтение файла гороскопов и передача словаря гороскопов в переменную
+        with open('astrological_predictions.json', 'r') as self.file:
             self.predictions = json.load(self.file)
 
-    def get_updates(self, url):
+    def get_updates(self):
         """Метод запрашивает обновление у сервера Telegram"""
 
-        get_updates_url = url + 'getupdates'
+        get_updates_url = self.url + 'getupdates'
         self.request = requests.get(get_updates_url)
-        print(self.request.json())
+        return self.request.json()
 
     def get_random_prediction(self, list_prediction):
         """Метод случайно выбирает гороскоп из списка"""
         return random.choice(list_prediction)
 
+    def request_processing(self):
+        """Метод обрабатывает входящие запросы"""
+        self.data = self.get_updates()
+
+        # Проверяем наличие новых сообщений.
+        for index in range(len(self.data["result"])):
+            self.last_request = self.data["result"][index]
+            self.update_id = self.last_request["update_id"]
+            if self.update_id > self.last_update_id:
+                self.chat_id = self.last_request["message"]["chat"]["id"]
+                self.message_text = self.last_request["message"]["text"]
+                self.my_ansver = self.message_processing(self.message_text)
+                self.send_ansver(self.chat_id, self.my_ansver)
+                self.last_update_id = self.update_id
+                with open('updates_id.txt', 'w') as self.updates_file:
+                    self.updates_file.write(str(self.update_id))
+
+    def message_processing(self, message: str):
+        """Метод обрабатывает сообщения от пользователей"""
+        if message == "/today":
+            return self.get_random_prediction(self.predictions["daily"])
+        elif message == "/tommorow":
+            return self.get_random_prediction(self.predictions["daily"])
+        elif message == "/week":
+            return self.get_random_prediction(self.predictions["weekly"])
+        elif message == "/month":
+            return self.get_random_prediction(self.predictions["monthly"])
+        elif message == "/help":
+            return self.predictions["help"]
+        else:
+            return "Введите /help"
+
+    def send_ansver(self, chat_id: int, ansver: str):
+        send_url = self.url + 'sendmessage?chat_id={}&text={}'.format(
+            chat_id, ansver)
+        requests.get(send_url)
+
 
 def main():
     """Метод выполнения модуля"""
     astrolog = AstrologBot()
-    # astrolog.get_updates(astrolog.url)
-    print(astrolog.get_random_prediction(astrolog.predictions['daily']))
+    while True:
+        astrolog.get_updates()
+        astrolog.request_processing()
 
 
 if __name__ == '__main__':
-    main()
+        main()
